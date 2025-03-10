cfaaace68c3d8f8a5c392ecc6f19fa9a3c49fc51
Ajyol Dhamala <ajyold112@gmail.com>
2025-03-10 09:13:39 -0500

    Review and payment entities

---
+++ Selu383.SP25.P03.Api/Features/Reviews/ReviewDto.cs
@@ -0,0 +1,13 @@
+﻿using Selu383.SP25.P03.Api.Features.Users;
+
+namespace Selu383.SP25.P03.Api.Features.Reviews
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public string review { get; set; }
+        public int Rating { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
