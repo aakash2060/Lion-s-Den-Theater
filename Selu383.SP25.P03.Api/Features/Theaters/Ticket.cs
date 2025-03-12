9ae434043d3db16bcd6f8f4f1ef7147d8838860d
Aakash Poudel <aakash.poudel.900@gmail.com>
2025-03-12 15:43:21 -0500

    Ticket and TicketDto added

---
+++ Selu383.SP25.P03.Api/Features/Theaters/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Selu383.SP25.P03.Api.Features.Movies;
+
+namespace Selu383.SP25.P03.Api.Features.Theaters
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int ShowtimeId { get; set; }
+        //public string UserId { get; set; }
+        [Required] public DateTime PurchaseDate { get; set; }
+        public string SeatNumber { get; set; }
+        [Required] public decimal Price { get; set; }
+        public bool IsChkeckedIn {  get; set; }
+        public string TicketType { get; set; } // adult or child
+        public string ConfirmationNumber { get; set; }
+        public Showtime Showtime { get; set; }
+        //public User User { get; set; }
+        //public Movie PosterUrl { get; set; }
+
+
+
+
+
+    }
+}
---
+++ Selu383.SP25.P03.Api/Features/Theaters/TicketDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Selu383.SP25.P03.Api.Features.Theaters
+{
+    public class TicketDto
+    {
+        public int Id { get; set; }
+        public int ShowtimeId { get; set; }
+        //public string UserId { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public string SeatNumber { get; set; }
+        public decimal Price { get; set; }
+        public bool IsCheckedIn { get; set; }
+        public string TicketType { get; set; }
+        public string ConfirmationCode { get; set; }
+        public string MovieTitle { get; set; }
+        public string TheaterName { get; set; }
+        public int HallNumber { get; set; }
+        public DateTime ShowtimeStart { get; set; }
+    }
+
+    public class TicketDetailDto : TicketDto
+    {
+        public string MoviePoster { get; set; }
+        public DateTime ShowtimeEnd { get; set; }
+        public bool Is3D { get; set; }
+        public string SpecialEvent { get; set; }
+    }
+
+    public class PurchaseTicketDto
+    {
+        public int ShowtimeId { get; set; }
+        public string SeatNumber { get; set; }
+        public string TicketType { get; set; } = "Adult";
+    }
+}
