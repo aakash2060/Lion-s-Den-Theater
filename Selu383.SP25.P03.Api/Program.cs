0dfdc30144920aac058eca61ef402dfcb0384fcf
Aakash Poudel <aakash.poudel.900@gmail.com>
2025-03-14 07:39:56 -0500

    Movies Seeded

--- Selu383.SP25.P03.Api/Program.cs
+++ Selu383.SP25.P03.Api/Program.cs
@@ -76,6 +76,7 @@ namespace Selu383.SP25.P03.Api
                 SeedTheaters.Initialize(scope.ServiceProvider);
                 await SeedRoles.Initialize(scope.ServiceProvider);
                 await SeedUsers.Initialize(scope.ServiceProvider);
+                SeedMovies.Initialize(scope.ServiceProvider);
             }
 
             // Configure the HTTP request pipeline.
