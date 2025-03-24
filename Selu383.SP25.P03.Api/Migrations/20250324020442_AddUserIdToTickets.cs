c99f74c2f6efe14de78c08881ce9827f1c33244c
Aakash Poudel <aakash.poudel.900@gmail.com>
2025-03-23 21:05:31 -0500

    migration to add id to tickets

---
+++ Selu383.SP25.P03.Api/Migrations/20250324020442_AddUserIdToTickets.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Selu383.SP25.P03.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToTickets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsChkeckedIn",
+                table: "Tickets");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsChkeckedIn",
+                table: "Tickets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
