98d694c032be91954e01ffd29d94bb2d2c4ba733
Ajyol Dhamala <ajyold112@gmail.com>
2025-04-08 14:08:10 -0500

    test fix x1

--- Selu383.SP25.P03.Api/Program.cs
+++ Selu383.SP25.P03.Api/Program.cs
@@ -93,11 +93,10 @@ namespace Selu383.SP25.P03.Api
                 SeedTheaters.Initialize(scope.ServiceProvider);
                 await SeedRoles.Initialize(scope.ServiceProvider);
                 await SeedUsers.Initialize(scope.ServiceProvider);
-                SeedMovies.Initialize(scope.ServiceProvider);
-                SeedFoodData.Initialize(scope.ServiceProvider);
                 SeedHalls.Initialize(scope.ServiceProvider);
+                SeedMovies.Initialize(scope.ServiceProvider);
                 SeedShowtimes.Initialize(scope.ServiceProvider);
-                SeedTickets.Initialize(scope.ServiceProvider);
+                SeedFoodData.Initialize(scope.ServiceProvider);
             }
 
             // Swagger Setup
